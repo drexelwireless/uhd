9f1721192bd4a33b56ab513800bb1aada8145a42
Geoffrey Mainland <mainland@apeiron.net>
2023-06-18 16:31:43 -0400

    Fix compiler warnings about body of for loop.

--- host/lib/usrp/e100/clock_ctrl.cpp
+++ host/lib/usrp/e100/clock_ctrl.cpp
@@ -86,13 +86,25 @@ struct clock_settings_type{
 };
 
 //! gives the greatest divisor of num between 1 and max inclusive
-template<typename T> static inline T greatest_divisor(T num, T max){
-    for (T i = max; i > 1; i--) if (num%i == 0) return i; return 1;
+template<typename T> static inline T greatest_divisor(T num, T max)
+{
+    for (T i = max; i > 1; i--) {
+        if (num % i == 0) {
+            return i;
+        }
+    }
+    return 1;
 }
 
 //! gives the least divisor of num between min and num exclusive
-template<typename T> static inline T least_divisor(T num, T min){
-    for (T i = min; i < num; i++) if (num%i == 0) return i; return 1;
+template<typename T> static inline T least_divisor(T num, T min)
+{
+    for (T i = min; i < num; i++) {
+        if (num % i == 0) {
+            return i;
+        }
+    }
+    return 1;
 }
 
 static clock_settings_type get_clock_settings(double rate){
